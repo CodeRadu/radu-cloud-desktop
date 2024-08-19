2c54a29004a7e7c266c6be9642316a552dd3ca91
Radu <47945947+CodeRadu@users.noreply.github.com>
2024-08-19 11:12:17 +0000

    Switch to NSIS

--- src/main.js
+++ src/main.js
@@ -1,16 +1,6 @@
-const { app, BrowserWindow, autoUpdater, dialog } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
+const { autoUpdater } = require('electron-updater');
 const path = require('path');
-
-const server = 'https://rcd-update-coderadu.vercel.app'
-const url = `${server}/update/${process.platform}/${app.getVersion()}`
-
-autoUpdater.setFeedURL({ url })
-
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
-  // eslint-disable-line global-require
-  app.quit();
-}
 
 const createWindow = () => {
   // Create the browser window.
@@ -25,32 +15,11 @@ const createWindow = () => {
   if (process.env.DEV === "true" && !app.isPackaged) {
     mainWindow.loadURL("http://localhost:3000")
     mainWindow.webContents.openDevTools();
-    console.log("Updates are be disabled while in development mode")
+    console.log("Updates are disabled while in development mode")
   }
   else {
     mainWindow.loadFile(path.join(__dirname, '../lib/radu-cloud/build/index.html'));
-    autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-      const dialogOpts = {
-        type: 'info',
-        buttons: ['Restart', 'Later'],
-        title: 'Application Update',
-        message: process.platform === 'win32' ? releaseNotes : releaseName,
-        detail:
-          'A new version has been downloaded. Restart the application to apply the updates.',
-      }
-
-      dialog.showMessageBox(dialogOpts).then((returnValue) => {
-        if (returnValue.response === 0) autoUpdater.quitAndInstall()
-      })
-    })
-    autoUpdater.on('error', (message) => {
-      console.error('There was a problem updating the application')
-      console.error(message)
-    })
-    autoUpdater.checkForUpdates()
-    setInterval(() => {
-      autoUpdater.checkForUpdates()
-    }, 300000)
+    autoUpdater.checkForUpdatesAndNotify();
   }
   // and load the index.html of the app.
 
